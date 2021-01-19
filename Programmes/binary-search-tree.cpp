86d851dcd85739381ace97c38d2f7556d2562b19
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-19 17:12:57 +0600

    binary search tree

---
+++ Programmes/binary-search-tree.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+struct btnode
+{
+    int value;
+    struct btnode *l;
+    struct btnode *r;
+}*root=NULL,*temp=NULL,*t1,*t2;
+void delete1();
+void insert();
+void delete();
+void inorder(struct btnode *t);
+void create();
+void search(struct btnode *t);
+void preorder(struct btnode *t);
+void postoredr(struct btnode *t);
+void search1(struct btnode *t,int data);
+int smallest(struct btnode *t);
+int largest(struct btnode *t);
+int flag=1;
+void main()
+{
+    int ch;
+    cout<<"OPERATIONS"<<endl;
+    cout<<"1.Insert An Element into tree"<<endl;
+    cout<<"2.Delete An Element from tree"<<endl;
+    cout<<"3.Inorder Traversal"<<endl;
+    cout<<"4.Preorder Tarversal"<<endl;
+    cout<<"5.Postorder Tarversal"<<endl;
+    cout<<"6.Exit"<<endl;
+    while(1)
+    {
+        cout<<"Enter Choice"<<endl;
+        cin>>ch;
+        switch(ch)
+        {
+        case 1:
+            insert();
+            break;
+        case 2:
+            delete();
+            break;
+        case 3:
+            inorder(root);
+            break;
+        case 4:
+            preorder(root);
+            break;
+        case 5:
+            postorder(root);
+            break;
+        case 6:
+            exit(0);
+        default:
+            cout<<"Wrong Choice"<<endl;
+            break;
+        }
+    }
+
+
+}
