ae96e9e275a033363a80a7332bb146becbb73656
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-20 20:12:21 +0600

    Hash Table  created

---
+++ Programmes/hash-table.cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+#define SIZE 20
+using namespace std;
+struct DataItem
+{
+    int data;
+    int key;
+};
+struct DataItem* hashArray[SIZE];
+struct DataItem* dummyItem;
+struct DataItem*  item;
+
+int hashCode(int key)
+{
+    return key%SIZE;
+}
+
+struct DataItem *search(int key)
+{
+    int hashIndex=hashCode(key);
+    while(hashArray[hashIndex]!=NULL)
+    {
+        if(hasArray[hashIndex]->key==key)
+        {
+            return hashArray[hashIndex];
+            ++hashIndex;
+        }
+        hashIndex%=SIZE;
+    }
+    return NULL;
+};
+void insert(int key,int data)
+{
+    struct DataItem *item=(struct DataItem)malloc(sizeof(struct DataItem));
+    item->data=data;
+    item->key=key;
+
+    int hashIndex=hashCode(key);
+    while(hashArray[hashIndex]!=NULL && hashArray[hashIndex]->key!=-1)
+    {
+        ++hashIndex;
+        hashIndex%=SIZE;
+    }
+    hashArray[hashIndex]=item;
+}
+
+struct DataItem* delete(struct DataItem* item)
+{
+    int key=item->key;
+    int hashIndex=hashCode(key);
+    while(hashArray[hashIndex]!=NULL)
+    {
+        if(hashArray[hashIndex]->key==key)
+        {
+            struct DataItem* temp=hashArray[hashIndex];
+            hashArray[hashIndex]=dummyItem;
+            return temp;
+        }
+        ++hashIndex;
+        hashIndex%=SIZE;
+
+    }
+    return NULL;
+}
+
+void display()
+{
+    int i=0;
+    for(i=0;i<SIZE;i++)
+    {
+
+        if(hashArray[i]!=NULL)
+        {
+            cout<<hashArray[i]->key,hashArray[i]->data;
+        }
+        else
+        {
+            cout<<"~~"<<endl;
+        }
+        cout<<"\n"<<endl;
+    }
+}
+
+int main()
+{
+    dummyItem=(struct DataItem*)malloc(sizeof(struct DataItem));
+    dummyItem->data=-1;
+    dummyItem->key=-1;
+    insert(1,20);
+    insert(2,70);
+    insert(42,80);
+    insert(4,25);
+    insert(12,44);
+    insert(14,32);
+    insert(17,11);
+    insert(13,78);
+    insert(37,97);
+    display();
+    item=search(37);
+    if(item!=NULL)
+    {
+        cout<<item->data;
+    }
+    else
+    {
+        cout<<"Element Not Found"<<endl;
+    }
+    delete(item);
+    item=search(37);
+     if(item!=NULL)
+    {
+        cout<<item->data;
+    }
+    else
+    {
+        cout<<"Element Not Found"<<endl;
+    }
+
+}
